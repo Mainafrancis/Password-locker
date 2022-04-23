74c350f7fb5895eabaa2190104d7ca1bf8dab557
Mainafrancis <francis.maina@student.moringaschool.com>
2022-04-23 23:53:51 +0300

    commandline

--- run.py
+++ run.py
@@ -7,3 +7,6 @@ def main():
     while True:
         print("welcome to password locker!!!")
         print('\n')
+        print("Select a short code to navigate through:to create new user use 'nu':To login to your account 'lg' or 'ex' to exit")
+        short_code = input().lower()
+        print('\n')
