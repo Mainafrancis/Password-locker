a2bfeaabbc4eb7306ff805b68f0199f4af33a076
Mainafrancis <francis.maina@student.moringaschool.com>
2022-04-23 23:09:26 +0300

    user details

---
+++ user.py
@@ -0,0 +1,22 @@
+from typing_extensions import Self
+
+
+class User:
+
+    '''
+    class that generates new instance of user
+    '''
+
+    user_list = []
+
+    def_init_(self,user_name,password):
+    Self.user_name = user_name
+    Self.password = Password
+
+    def save_user(self):
+
+        '''
+        save_user method saves a new user objects to the user_list
+        '''
+
+        User.user_list.append(self)
