f5e52f2738e8b2cd2e4db702390082c287fded3e
Mainafrancis <francis.maina@student.moringaschool.com>
2022-04-24 00:01:48 +0300

    input

--- run.py
+++ run.py
@@ -10,3 +10,10 @@ def main():
         print("Select a short code to navigate through:to create new user use 'nu':To login to your account 'lg' or 'ex' to exit")
         short_code = input().lower()
         print('\n')
+
+        if short_code == 'nu':
+            print('create username')
+            created_user_name = input()
+
+            print('create password')
+            created_user_password = input()
