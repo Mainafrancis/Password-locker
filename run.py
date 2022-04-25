9199c14ef3c53a802c97b0db4fabb11698ae58c4
Mainafrancis <francis.maina@student.moringaschool.com>
2022-04-25 09:39:22 +0300

    text display

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ from user import User
 def main():
 
     while True:
-        print("welcome to password locker!!!")
+        print("Hey there, welcome to secret launcher!!!")
         print('\n')
-        print("Select a short code to navigate through:to create new user use 'nu':To login to your account 'lg' or 'ex' to exit")
+        print("To create new user use 'nu':To login to your account 'lg' or 'ex' to exit")
         short_code = input().lower()
         print('\n')
 
